8b43c8e3808f7abd2a281691b0fc1312062c1bc1
penrose15 <seongju99@naver.com>
2023-11-12 16:31:44 +0900

    Feat: main메서드에 실행 로직 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.domain.EventController;
+import christmas.domain.calendar.EventCalendar;
+import christmas.domain.order.Orders;
+import christmas.domain.sale.Sale;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Orders orders = new Orders();
+        EventCalendar eventCalendar = new EventCalendar();
+
+        EventController eventController = new EventController(orders, eventCalendar);
+
+        eventController.play();
     }
 }
